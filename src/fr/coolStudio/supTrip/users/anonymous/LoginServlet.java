8aeca9c3f96ea772e1cf5f27e74222210eed2155
Mike <me@sayfus>
2015-11-27 22:02:07 +0100

    Clean up, +LoginServlet

---
+++ src/fr/coolStudio/supTrip/users/anonymous/LoginServlet.java
@@ -0,0 +1,35 @@
+package fr.coolStudio.supTrip.users.anonymous;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+@WebServlet(
+	description = "Gère la connexion des utilisateurs et tout ce qui est lié à la fonctionnalité (formulaires, requetes, verifications, ...)",
+	urlPatterns = { "/login" }
+)
+public class LoginServlet extends HttpServlet
+{
+	private static final long serialVersionUID = 211L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	throws ServletException, IOException
+	{
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/users/anonymous/login.html");
+		dispatcher.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	throws ServletException, IOException
+	{
+		response.getWriter().print("We got you request but the server is temporaly under maintenance, try later :)");
+	}
+
+}
