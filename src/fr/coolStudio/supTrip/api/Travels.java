7600d45c8cb6cafc26d19b93bca580dd32368bb1
Mike <me@sayfus>
2015-11-28 18:51:31 +0100

    ~API v0.2 (subject conforming, check with team)

--- src/fr/coolStudio/supTrip/api/Travels.java
+++ src/fr/coolStudio/supTrip/api/Travels.java
@@ -7,20 +7,26 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 
 @Path("/travels")
+@Produces(MediaType.APPLICATION_JSON)
 public class Travels
 {
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
     public String getTravels()
 	{
-        return "{allTravels: null}";
+        return "{\n\tallTravels: null\n}";
     }
 
 	@GET
 	@Path("/{id: [0-9]+}")
-	@Produces(MediaType.APPLICATION_JSON)
     public String getTravel(@PathParam("id") int tid)
 	{
-        return "{Hey : " + tid + "}";
+        return "{\n\tHey : " + tid + "\n}";
+    }
+
+	@GET
+	@Path("/{departure: [A-Za-z]*}/{destination: [A-Za-z]*}")
+    public String searchTravel(@PathParam("departure") String from, @PathParam("destination") String to)
+	{
+        return "{\n\tdeparture : \"" + from + "\",\n\tdestination : \"" + to + "\"\n}";
     }
 }
